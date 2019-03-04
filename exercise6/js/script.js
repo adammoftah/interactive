0e13a6e9e605f1aa3463c332660e3679d6eaf2e1
Adam Moftah <adammoftah@Adams-MacBook-Pro-5.local>
2019-03-04 13:43:10 -0500

    added exercise 6, potentially again

---
+++ exercise6/js/script.js
@@ -0,0 +1,25 @@
+// $("#crest").fadeOut();
+// $("body").addClass("gradient");
+// $("#status").fadeIn("slow");
+// var stop = true;
+
+
+// $("#duck").click(function(){
+// 	console.log("cloick")
+// 	//$("talkbubble").css("display", "show");
+// })
+
+// var quack = false;
+
+// setInterval(function(){	
+
+// 		$("#talkbubble").css("display", "show");
+// }
+
+// 	}
+// 	else{
+// 		console.log("haaa")
+// 		$("#status").html("Stop");
+// 		$("#status").css("background", "red");
+// 		stop = true;
+// 	}}, 10000);
